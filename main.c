442caa071b2e0ab9cae79a01799ec40bc0cb2c88
Goenae <61237031+Goenae@users.noreply.github.com>
2023-03-01 19:32:44 +0100

    Beautify code, structure functions

--- main.c
+++ main.c
@@ -3,29 +3,70 @@
 #include <mysql/mysql.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+MYSQL *getDatabaseConnection(){
     MYSQL *conn;
-    int choix;
-    bool loop = true;
 
     if ((conn = mysql_init(NULL)) == NULL) {
         fprintf(stderr, "Initialisation failed");
         exit(1);
     }
+    return conn;
+}
 
-    if(mysql_real_connect(conn, "localhost", "mike", "jgldiff", "ip", 0, NULL, 0) == NULL) {
+void connectToDatabase(MYSQL *connection){
+    if(mysql_real_connect(connection, getenv("DB_HOST"), getenv("DB_USER"), getenv("DB_PASSWORD"), getenv("DB_NAME"), 0, NULL, 0) == NULL) {
         fprintf(stderr, "Connection failed");
         exit(1);
     }
+}
+
+void displayIpAddresses(char format, const char* mask){
+    MYSQL *conn = getDatabaseConnection();
+    if(strcmp(mask, "0") != 0){
+        printf("Mask filtering");
+    }else{
+        if (mysql_query(conn, "SELECT * FROM ips")) {
+            fprintf(stderr, "Select failed");
+            exit(1);
+        }
+        MYSQL_RES *result = mysql_store_result(conn);
+
+        if (result == NULL) {
+            fprintf(stderr, "Select failed");
+            exit(1);
+        }
+
+        int num_fields = mysql_num_fields(result);
+
+        MYSQL_ROW row;
+
+        while ((row = mysql_fetch_row(result))) {
+            for(int i = 0; i < num_fields; i++) {
+                printf("%s ", row[i] ? row[i] : "NULL");
+            }
+            printf("\n");
+        }
+
+        mysql_free_result(result);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int choix;
+    bool loop = true;
+
+    MYSQL *conn = getDatabaseConnection();
+    connectToDatabase(conn);
 
     while(loop != false){
-        printf("Bienvenu dans le répertoire d\'adresses IP\n");
+        printf("Bienvenue dans le répertoire d\'adresses IP\n");
         printf("Faites votre choix:\n");
         printf("1. Ajouter une adresse IP\n");
         printf("2. Supprimer une adresse IP\n");
         printf("3. Afficher les adresses IP\n");
         printf("4. Quitter\n");
 
+        fflush(stdin);
         scanf("%d", &choix);
 
         switch(choix) {
@@ -45,32 +86,25 @@ int main(int argc, char *argv[]) {
                 printf("Vous avez choisi de supprimer une adresse IP\n");
                 break;
             case 3:
-                printf("Vous avez choisi d\'afficher les adresses IP\n");
+                //printf("Vous avez choisi d\'afficher les adresses IP\n");
+                printf("Sous quel format voulez-vous les afficher ?\n");
+                printf("Faites votre choix:\n");
+                printf("1. Sous forme binaire\n");
+                printf("2. Sous forme hexadecimale\n");
+                printf("3. Sous forme décimale\n");
+                printf("4. Quitter\n");
 
-                if (mysql_query(conn, "SELECT * FROM ips")) {
-                    fprintf(stderr, "Select failed");
-                    exit(1);
-                }
+                char* choix2;
 
-                MYSQL_RES *result = mysql_store_result(conn);
+                fflush(stdin);
+                scanf("%d", &choix);
 
-                if (result == NULL) {
-                    fprintf(stderr, "Select failed");
-                    exit(1);
-                }
+                printf("Insérez un masque pour filtrer (ou 0 si aucun):");
 
-                int num_fields = mysql_num_fields(result);
+                fflush(stdin);
+                scanf("%s", &choix2);
 
-                MYSQL_ROW row;
-
-                while ((row = mysql_fetch_row(result))) {
-                    for(int i = 0; i < num_fields; i++) {
-                        printf("%s ", row[i] ? row[i] : "NULL");
-                    }
-                    printf("\n");
-                }
-
-                mysql_free_result(result);
+                displayIpAddresses(choix, choix2);
 
                 break;
             case 4:

adb5c5117d940d1e65ed5d16f4c084a90ba50f41
Goenae <61237031+Goenae@users.noreply.github.com>
2023-03-03 18:05:19 +0100

    WIP Insert function, enhance UI

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ void connectToDatabase(MYSQL *connection){
     }
 }
 
-void displayIpAddresses(char format, const char* mask){
+void displayIpAddresses(int format, const char* mask){
     MYSQL *conn = getDatabaseConnection();
     if(strcmp(mask, "0") != 0){
         printf("Mask filtering");
@@ -40,6 +40,7 @@ void displayIpAddresses(char format, const char* mask){
 
         MYSQL_ROW row;
 
+        system("clear");
         while ((row = mysql_fetch_row(result))) {
             for(int i = 0; i < num_fields; i++) {
                 printf("%s ", row[i] ? row[i] : "NULL");
@@ -51,6 +52,19 @@ void displayIpAddresses(char format, const char* mask){
     }
 }
 
+void insertIpToDatabase(char ip_address[]){
+    MYSQL *conn = getDatabaseConnection();
+    char buf[1024] = {};
+    char query_string[] = {
+            "INSERT INTO ips(ip) VALUES(%s)"
+    };
+    sprintf(buf, query_string, ip_address);
+    if (mysql_query(conn, buf)) {
+        fprintf(stderr, "Insertion failed");
+        exit(1);
+    }
+}
+
 int main(int argc, char *argv[]) {
     int choix;
     bool loop = true;
@@ -59,6 +73,7 @@ int main(int argc, char *argv[]) {
     connectToDatabase(conn);
 
     while(loop != false){
+        system("clear");
         printf("Bienvenue dans le répertoire d\'adresses IP\n");
         printf("Faites votre choix:\n");
         printf("1. Ajouter une adresse IP\n");
@@ -71,22 +86,21 @@ int main(int argc, char *argv[]) {
 
         switch(choix) {
             case 1:
-                printf("Ajouter une adresse IP\n");
+                system("clear");
+                printf("Veuillez saisir une adresse ip:\n");
                 char ip[20];
-                //scanf("%s", &ip);
-
-                if (mysql_query(conn, ip)) {
-                    fprintf(stderr, "Insertion failed");
-                    exit(1);
-                }
+                scanf("%s", ip);
+                insertIpToDatabase(ip);
 
                 printf("Insertion réussie\n");
                 break;
             case 2:
+                system("clear");
                 printf("Vous avez choisi de supprimer une adresse IP\n");
                 break;
             case 3:
                 //printf("Vous avez choisi d\'afficher les adresses IP\n");
+                system("clear");
                 printf("Sous quel format voulez-vous les afficher ?\n");
                 printf("Faites votre choix:\n");
                 printf("1. Sous forme binaire\n");
@@ -94,24 +108,28 @@ int main(int argc, char *argv[]) {
                 printf("3. Sous forme décimale\n");
                 printf("4. Quitter\n");
 
-                char* choix2;
+
 
                 fflush(stdin);
                 scanf("%d", &choix);
 
+                system("clear");
                 printf("Insérez un masque pour filtrer (ou 0 si aucun):");
 
                 fflush(stdin);
-                scanf("%s", &choix2);
+                char choix2[15];
+                scanf("%s", choix2);
 
                 displayIpAddresses(choix, choix2);
 
                 break;
             case 4:
+                system("clear");
                 printf("Vous avez choisi de quitter\n");
                 loop = false;
                 break;
             default:
+                system("clear");
                 printf("Choix invalide\n");
                 break;
         }
